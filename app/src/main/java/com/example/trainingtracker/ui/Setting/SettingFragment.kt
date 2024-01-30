40858c28394d8a11f9a118a2eebf47c7ce942f6c
JeeIn-Park <1700pji@naver.com>
2024-01-30 22:53:20 +0000

    dashboard -> status, notification -> setting

--- app/src/main/java/com/example/trainingtracker/ui/Setting/SettingFragment.kt
+++ app/src/main/java/com/example/trainingtracker/ui/Setting/SettingFragment.kt
@@ -1,4 +1,4 @@
-package com.example.trainingtracker.ui.dashboard
+package com.example.trainingtracker.ui.Setting
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.trainingtracker.databinding.FragmentDashboardBinding
+import com.example.trainingtracker.databinding.FragmentSettingBinding
 
-class DashboardFragment : Fragment() {
+class SettingFragment : Fragment() {
 
-private var _binding: FragmentDashboardBinding? = null
+private var _binding: FragmentSettingBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
@@ -21,14 +21,14 @@ private var _binding: FragmentDashboardBinding? = null
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    val settingViewModel =
+            ViewModelProvider(this).get(SettingViewModel::class.java)
 
-    _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+    _binding = FragmentSettingBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textDashboard
-    dashboardViewModel.text.observe(viewLifecycleOwner) {
+    val textView: TextView = binding.textSetting
+      settingViewModel.text.observe(viewLifecycleOwner) {
       textView.text = it
     }
     return root
--- app/src/main/java/com/example/trainingtracker/ui/Status/StatusViewModel.kt
+++ app/src/main/java/com/example/trainingtracker/ui/Status/StatusViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.trainingtracker.ui.dashboard
+package com.example.trainingtracker.ui.Status
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class StatusViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
