fe124dcdfa1d2fc120da7a6cff5e357b845a0b67
JeeIn-Park <1700pji@naver.com>
2024-04-11 20:53:38 +0100

    status tag bar

--- app/src/main/java/com/example/trainingtracker/ui/status/StatusFragment.kt
+++ app/src/main/java/com/example/trainingtracker/ui/status/StatusFragment.kt
@@ -1,7 +1,6 @@
 package com.example.trainingtracker.ui.status
 
 import android.app.AlertDialog
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,23 +11,17 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.trainingtracker.R
-import com.example.trainingtracker.ui.exerciseCard.AddCardActivity
-import com.example.trainingtracker.ui.exerciseLog.AddLogActivity
 import com.example.trainingtracker.ui.exerciseCard.CardStorage
 import com.example.trainingtracker.databinding.FragmentStatusBinding
 import com.example.trainingtracker.ui.tag.Tag
 import com.example.trainingtracker.ui.tag.TagAdapter
+import com.example.trainingtracker.ui.tag.TagFactory
 import com.example.trainingtracker.ui.tag.TagStorage
-import java.time.LocalDateTime
-import java.util.UUID
 
 class StatusFragment : Fragment() {
 
     private var _binding: FragmentStatusBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-    private lateinit var cardAdapter: StatusCardAdapter
     private lateinit var tagAdapter: TagAdapter
 
   override fun onCreateView(
@@ -40,7 +33,63 @@ class StatusFragment : Fragment() {
           ViewModelProvider(this).get(StatusViewModel::class.java)
       _binding = FragmentStatusBinding.inflate(inflater, container, false)
       val root: View = binding.root
+
+      tagAdapter = TagAdapter(requireContext()) { clickedTag ->
+          if (clickedTag.name == Tag.ADD_TAG.name) {
+              val inputDialog = AlertDialog.Builder(requireContext())
+              val inputEditText = EditText(requireContext())
+              inputDialog.setView(inputEditText)
+              inputDialog.setTitle(getString(R.string.tag_enter_name))
+
+              inputDialog.setPositiveButton("OK") { dialog, _ ->
+                  val newTagString = inputEditText.text.toString().trim()
+                  if (newTagString.isNotEmpty()) {
+                      TagStorage.addTag(requireContext(), TagFactory.createTag(requireContext(), newTagString))
+                      refresh()
+                  }
+                  dialog.dismiss()
+              }
+
+              inputDialog.setNegativeButton("Cancel") { dialog, _ ->
+                  dialog.dismiss()
+              }
+
+              inputDialog.show()
+          } else {
+              tagAdapter.editItem(clickedTag, TagFactory.clickTag(clickedTag))
+              refresh()
+          }
+      }
+
+      val tagRecyclerViewBinding = binding.filterBar.tagRecyclerView
+      tagRecyclerViewBinding.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+      tagRecyclerViewBinding.adapter = tagAdapter
+      tagRecyclerViewBinding.itemAnimator = DefaultItemAnimator()
+      statusViewModel.tagRecyclerViewData.observe(viewLifecycleOwner) { newData ->
+          tagAdapter.submitList(newData)
+          statusViewModel.updateTagRecyclerViewData(newData)
+      }
+
       return root
   }
+
+    override fun onResume() {
+        super.onResume()
+        refresh()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun refresh() {
+        val tags : MutableList<Tag> = TagStorage.loadTags(requireContext()).toMutableList()
+        tags.removeAll { it == Tag.ADD_TAG }
+        tags.add(Tag.ADD_TAG)
+        tagAdapter.submitList(tags)
+        val selectedTags = TagStorage.getSelectedTags(requireContext())
+        val cards = CardStorage.getSelectedCard(requireContext(), selectedTags)
+    }
 
 }
