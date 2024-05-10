898d346fc990edb3cab280cf116a2e9f5ea0e77c
JeeIn-Park <1700pji@naver.com>
2024-05-11 00:39:54 +0100

    changed the report

--- app/src/main/java/com/jeein/trainingtracker/ui/setting/ReportingHandler.kt
+++ app/src/main/java/com/jeein/trainingtracker/ui/setting/ReportingHandler.kt
@@ -12,41 +12,41 @@ import java.io.IOException
 import java.io.InputStreamReader
 
 object ReportingHandler {
-    private fun getLogcatData(): String {
-        try {
-            val process = Runtime.getRuntime().exec("logcat -d -v time MyAppTag:* *:S")
-            val bufferedReader = BufferedReader(InputStreamReader(process.inputStream))
+//    private fun getLogcatData(): String {
+//        try {
+//            val process = Runtime.getRuntime().exec("logcat -d -v time MyAppTag:* *:S")
+//            val bufferedReader = BufferedReader(InputStreamReader(process.inputStream))
+//
+//            val log = StringBuilder()
+//            var line: String?
+//            while (bufferedReader.readLine().also { line = it } != null) {
+//                log.append(line).append("\n")
+//            }
+//            return log.toString()
+//        } catch (ex: IOException) {
+//            Log.e("SettingFragment", "Error reading logcat", ex)
+//        }
+//        return "Unable to collect Logcat data."
+//    }
 
-            val log = StringBuilder()
-            var line: String?
-            while (bufferedReader.readLine().also { line = it } != null) {
-                log.append(line).append("\n")
-            }
-            return log.toString()
-        } catch (ex: IOException) {
-            Log.e("SettingFragment", "Error reading logcat", ex)
-        }
-        return "Unable to collect Logcat data."
-    }
 
-
-    private fun getUserData(context: Context): List<Uri> {
-        val filesToAttach = mutableListOf<Uri>()
-//        "log_${specificId}.dat",
-        // TODO : store list of id in separate place so it can ensure it has those ids
-        val fileNames = listOf("exercise_cards.dat", "muscles.dat", "tags.dat")
-
-        fileNames.forEach { fileName ->
-            val file = File(context.filesDir, fileName)
-            if (file.exists()) {
-                val fileUri = Uri.fromFile(file)
-                filesToAttach.add(fileUri)
-            } else {
-                Log.e("SettingsFragment", "File not found: $fileName")
-            }
-        }
-        return filesToAttach
-    }
+//    private fun getUserData(context: Context): List<Uri> {
+//        val filesToAttach = mutableListOf<Uri>()
+////        "log_${specificId}.dat",
+//        // TODO : store list of id in separate place so it can ensure it has those ids
+//        val fileNames = listOf("exercise_cards.dat", "muscles.dat", "tags.dat")
+//
+//        fileNames.forEach { fileName ->
+//            val file = File(context.filesDir, fileName)
+//            if (file.exists()) {
+//                val fileUri = Uri.fromFile(file)
+//                filesToAttach.add(fileUri)
+//            } else {
+//                Log.e("SettingsFragment", "File not found: $fileName")
+//            }
+//        }
+//        return filesToAttach
+//    }
 
 
     fun sendFeedbackByEmail(context: Context, feedback: String) {
@@ -64,18 +64,32 @@ object ReportingHandler {
     }
 
 
-    fun sendReportByEmail(context: Context, description: String) {
-        val logcatData = getLogcatData()
-        val userData = getUserData(context)
-        val emailBody =
-            "Error Description: $description\n\nLogcat Data:\n$logcatData\n\nUser Data: $userData"
+//    fun sendReportByEmail(context: Context, description: String) {
+//        val logcatData = getLogcatData()
+//        val userData = getUserData(context)
+//        val emailBody =
+//            "Error Description: $description\n\nLogcat Data:\n$logcatData\n\nUser Data: $userData"
+//
+//        val intent = Intent(Intent.ACTION_SEND).apply {
+//            type = "message/rfc822"
+//            putExtra(Intent.EXTRA_EMAIL, arrayOf(context.getString(R.string.feedback_mail)))
+//            putExtra(Intent.EXTRA_SUBJECT, "Error Report from App")
+//            putExtra(Intent.EXTRA_TEXT, emailBody)
+//            putParcelableArrayListExtra(Intent.EXTRA_STREAM, ArrayList(userData))
+//        }
+//        try {
+//            context.startActivity(Intent.createChooser(intent, "Send email using..."))
+//        } catch (ex: android.content.ActivityNotFoundException) {
+//            Toast.makeText(context, "No email clients installed.", Toast.LENGTH_SHORT).show()
+//        }
+//    }
 
+    fun sendReportByEmail(context: Context, description: String) {
         val intent = Intent(Intent.ACTION_SEND).apply {
             type = "message/rfc822"
             putExtra(Intent.EXTRA_EMAIL, arrayOf(context.getString(R.string.feedback_mail)))
-            putExtra(Intent.EXTRA_SUBJECT, "Error Report from App")
-            putExtra(Intent.EXTRA_TEXT, emailBody)
-            putParcelableArrayListExtra(Intent.EXTRA_STREAM, ArrayList(userData))
+            putExtra(Intent.EXTRA_SUBJECT, "Feedback from App")
+            putExtra(Intent.EXTRA_TEXT, description)
         }
         try {
             context.startActivity(Intent.createChooser(intent, "Send email using..."))
