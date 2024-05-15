76a1ae8b9a280854c7d5a208c05c511e84372b3b
JeeIn-Park <1700pji@naver.com>
2024-05-15 02:54:05 +0100

    version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jeein.trainingtracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "2.2.0"
+        versionCode = 4
+        versionName = "3.0.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
